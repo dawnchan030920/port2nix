c5cc7a3e4c9e164e460582d46e69656e2561db83
dawnchan030920 <dawnchan030920@outlook.com>
2024-01-05 17:38:02 +0000

    [init] nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "port2nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+  };
+
+  outputs = { self, nixpkgs, ...}:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_21
+            nodePackages.typescript-language-server
+            vscode-langservers-extracted
+            nil
+            marksman
+          ];
+        };
+      };
+    };
+}
